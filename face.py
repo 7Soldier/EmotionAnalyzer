9bd96bb76986b37c5dc978a242094f7348040cb0
7Soldier <reg.fm4@gmail.com>
2025-04-22 21:12:35 +0300

    face: Show video from web camera in real time
    Signed-off-by: 7Soldier <reg.fm4@gmail.com>

--- face.py
+++ face.py
@@ -20,50 +20,57 @@ from data import write_face_emotions
 
 # Определяем класс FaceEmotionDetector
 class FaceEmotionDetector:
-    # Инициализируем класс с очередью сообщений в качестве аргумента
     def __init__(self, message_queue):
-        # Загружаем каскад Хаара для обнаружения лиц
         self.face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-        self.video = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # Открываем видеопоток с веб-камеры
+        self.video = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        self.emotion_detector = FER(mtcnn=True)
+        self.emotions_counter = Counter()
+        self.message_queue = message_queue
 
-        self.emotion_detector = FER(mtcnn=True)  # Инициализируем детектор эмоций
-        self.emotions_counter = Counter()  # Инициализируем счетчик для подсчета эмоций
-        self.message_queue = message_queue  # Сохраняем очередь сообщений
-
-        # Если видеопоток не открыт, вызываем исключение
         if not self.video.isOpened():
             raise Exception("Cannot open webcam")
 
-    # Метод для обнаружения лиц на кадре
     def detect_faces(self, frame):
-        # Обесцвечиваем кадр
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # Обнаруживаем лица на кадре
         faces = self.face_cascade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=15)
-
         return faces
 
-    # Метод для обнаружения эмоций на лицах
-    def detect_emotion(self, frame, faces):
-        # Обнаруживаем эмоцию на лице
-        emotion = self.emotion_detector.top_emotion(frame)[0]
-        # Увеличиваем счетчик для этой эмоции
+    def detect_emotion(self, frame, face):
+        x, y, w, h = face
+        face_region = frame[y:y+h, x:x+w]
+        emotion, score = self.emotion_detector.top_emotion(face_region)
         self.emotions_counter[emotion] += 1
+        return emotion
 
-    # Основной метод для запуска детектора
     def main(self):
-        # Пока видеопоток открыт
         while self.video.isOpened():
-            # Если в очереди сообщений есть сообщения
             if not self.message_queue.empty():
-                message = self.message_queue.get()  # Получаем сообщение из очереди
-                # Если сообщение - 'face_exit'
+                message = self.message_queue.get()
                 if message == 'face_exit':
-                    write_face_emotions(self.emotions_counter)  # Записываем эмоции в файл
-                    self.video.release()  # Освобождаем видеопоток
-                    cv2.destroyAllWindows()  # Закрываем все окна
-                    break  # Прерываем цикл
+                    write_face_emotions(self.emotions_counter)
+                    self.video.release()
+                    cv2.destroyAllWindows()
+                    break
 
-            ret, frame = self.video.read()  # Читаем кадр из видеопотока
-            faces = self.detect_faces(frame)  # Обнаруживаем лица на кадре
-            self.detect_emotion(frame, faces)  # Обнаруживаем эмоции на лицах
+            ret, frame = self.video.read()
+            if not ret:
+                break
+
+            faces = self.detect_faces(frame)
+
+            for face in faces:
+                x, y, w, h = face
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+                emotion = self.detect_emotion(frame, face)
+                cv2.putText(frame, emotion, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+            cv2.imshow("Face Emotion Detector", frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                write_face_emotions(self.emotions_counter)
+                self.video.release()
+                cv2.destroyAllWindows()
+                break
+
+        self.video.release()
+        cv2.destroyAllWindows()
